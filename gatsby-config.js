bd4783e1fa10eb0e7997ac61601001834a44bbb4
shoonia <zaytsev126@gmail.com>
2020-05-08 23:31:41 +0300

    Select code

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,7 @@ module.exports = {
             resolve: 'gatsby-remark-prismjs',
             options: {
               noInlineHighlight: true,
+              classPrefix: 'code-',
             },
           },
           {
