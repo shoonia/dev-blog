64fa42d41c838c21303976ab8512b5561fea5d50
shoonia <zaytsev126@gmail.com>
2020-05-09 09:31:25 +0300

    Fix inline script

--- util/ga.js
+++ util/ga.js
@@ -1,10 +1,16 @@
 /* eslint-disable */
 
-(function (e, t, n, i, s, a, c) {
-  e[n] = e[n] || function () { (e[n].q = e[n].q || []).push(arguments); };
-  a = t.createElement(i); c = t.getElementsByTagName(i)[0]; a.async = true; a.src = s;
-  c.parentNode.insertBefore(a, c);
-}(window, document, 'galite', 'script', 'https://static.parastorage.com/unpkg/ga-lite@2.0.5/dist/ga-lite.min.js'));
+((d) => {
+  const s = d.createElement('script');
 
-galite('create', 'UA-137813864-1', 'auto');
-galite('send', 'pageview');
+  s.async = true;
+  s.src = 'https://static.parastorage.com/unpkg/ga-lite@2.0.5/dist/ga-lite.min.js';
+  s.crossOrigin = 'anonymous';
+  s.onload = () => {
+    galite('create', 'UA-137813864-1', 'auto');
+    galite('send', 'pageview');
+    s.onload = null;
+  };
+
+  d.body.appendChild(s);
+})(document);
