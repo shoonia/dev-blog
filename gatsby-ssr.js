5e6e41c0538ee9de022e01b495230bff5ca37dd8
shoonia <zaytsev126@gmail.com>
2020-06-22 22:06:04 +0300

    Remove GA

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,25 +1,5 @@
-const { createElement } = require('react');
 const { renderToString } = require('react-dom/server');
-const { minifyHTML, minifyJS } = require('./util/html.js');
-
-const code = minifyJS('./util/ga.js');
-const isProduction = process.env.NODE_ENV === 'production';
-
-exports.onRenderBody = ({ setPostBodyComponents }) => {
-  if (isProduction) {
-    setPostBodyComponents([
-      createElement('script', {
-        async: true,
-        id: 'ga-lite',
-        src: 'https://static.parastorage.com/unpkg/ga-lite@2.0.5/dist/ga-lite.min.js',
-        crossOrigin: 'anonymous',
-      }),
-      createElement('script', {
-        dangerouslySetInnerHTML: { __html: code },
-      }),
-    ]);
-  }
-};
+const { minifyHTML } = require('./util/html.js');
 
 exports.replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
   const bodyHTML = renderToString(bodyComponent);
--- util/html.js
+++ util/html.js
@@ -1,6 +1,4 @@
-const { readFileSync } = require('fs');
 const htmlMinifier = require('html-minifier');
-const tarser = require('terser');
 
 const htmlInifierOptions = {
   removeComments: true,
@@ -13,30 +11,6 @@ const htmlInifierOptions = {
   minifyURLs: true,
 };
 
-const tarserOptions = {
-  ecma: 8,
-  module: true,
-  toplevel: true,
-  parse: {
-    ecma: 8,
-  },
-  compress: {
-    ecma: 8,
-    module: true,
-    toplevel: true,
-    warnings: false,
-    comparisons: false,
-    inline: 2,
-    drop_console: true,
-    passes: 3,
-  },
-  output: {
-    ecma: 8,
-    comments: false,
-  },
-};
-
 module.exports = {
   minifyHTML: (html) => htmlMinifier.minify(html, htmlInifierOptions),
-  minifyJS: (path) => tarser.minify(readFileSync(path, 'utf8'), tarserOptions).code,
 };
