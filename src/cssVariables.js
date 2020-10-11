498c4adee0ec8e72e70cdc508fcebb8e22269dd8
shoonia <zaytsev126@gmail.com>
2020-10-11 23:39:10 +0300

    Remove CSS custom properties

--- src/cssVariables.js
+++ src/cssVariables.js
@@ -2,4 +2,6 @@ module.exports = {
   colorLink: 'rgb(26, 114, 232)',
   colorGray: 'rgba(0, 0, 0, 0.64)',
   contentWidht: '750px',
+  colorLightGray: 'rgb(112, 128, 144)',
+  colorLightGrayOpacity: 'rgba(112, 128, 144, 0.4)',
 };
