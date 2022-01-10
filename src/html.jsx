4cdb9ff20f313ce4435615c40260ed067285ec4e
shoonia <alexanderz@wix.com>
2022-01-10 16:02:16 +0200

    Refactofing of HTML file

--- src/html.jsx
+++ src/html.jsx
@@ -2,99 +2,84 @@ import T from 'prop-types';
 
 import { title, homepage, createUrl, isProd } from '../util/meta';
 
-const links = (
-  <>
-    <meta charSet="utf-8" />
-    <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,shrink-to-fit=no" />
-    <base href={homepage} />
-    <link
-      rel="preload"
-      href="/firacode@5.2.0/FiraCode-Regular.woff2"
-      as="font"
-      type="font/woff2"
-      crossOrigin="anonymous"
-    />
-    <link rel="webmention" href="https://webmention.io/shoonia.site/webmention" />
-    <link rel="pingback" href="https://webmention.io/shoonia.site/xmlrpc" />
-    <link
-      rel="alternate"
-      href={createUrl('rss.xml')}
-      type="application/rss+xml"
-      title={title}
-    />
-    <link
-      rel="alternate"
-      href={createUrl('rss.json')}
-      type="application/json"
-      title={title}
-    />
-    <link
-      rel="sitemap"
-      href="/sitemap.xml"
-      type="application/xml"
-      title={title}
-    />
-  </>
-);
-
-function HTML({
+const HTML = ({
   htmlAttributes,
   headComponents,
-  postBodyComponents,
+  bodyAttributes,
   body,
-}) {
-  // PRODUCTION
-  if (isProd) {
-    return (
-      <html lang={htmlAttributes.lang}>
-        <head>
-          {links}
-          {headComponents}
-          <script
-            type="module"
-            dangerouslySetInnerHTML={{
-              __html: 'import("/quicklink@2.2.0/quicklink.mjs").then(i=>i.listen())',
-            }}
-          />
-        </head>
-        <body
-          // eslint-disable-next-line react/no-danger
-          dangerouslySetInnerHTML={{ __html: body }}
-        />
-      </html>
-    );
-  }
-
-  // DEVELOPMENT
-  return (
-    <html lang={htmlAttributes.lang}>
-      <head>
-        {links}
-        {headComponents}
-      </head>
-      <body>
-        <div id="___gatsby">
+  postBodyComponents,
+}) => (
+  <html {...htmlAttributes}>
+    <head>
+      <meta charSet="utf-8" />
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,shrink-to-fit=no"
+      />
+      <base href={homepage} />
+      <link
+        rel="preload"
+        href="/firacode@5.2.0/FiraCode-Regular.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin="anonymous"
+      />
+      <link
+        rel="webmention"
+        href="https://webmention.io/shoonia.site/webmention"
+      />
+      <link
+        rel="pingback"
+        href="https://webmention.io/shoonia.site/xmlrpc"
+      />
+      <link
+        rel="alternate"
+        href={createUrl('rss.xml')}
+        type="application/rss+xml"
+        title={title}
+      />
+      <link
+        rel="alternate"
+        href={createUrl('rss.json')}
+        type="application/json"
+        title={title}
+      />
+      <link
+        rel="sitemap"
+        href="/sitemap.xml"
+        type="application/xml"
+        title={title}
+      />
+      {headComponents}
+      <script
+        type="module"
+        dangerouslySetInnerHTML={{
+          __html: 'import("/quicklink@2.2.0/quicklink.mjs").then(i=>i.listen())',
+        }}
+      />
+    </head>
+    {isProd
+      ? <body dangerouslySetInnerHTML={{ __html: body }} />
+      : (
+        <body {...bodyAttributes}>
           <div
-            id="gatsby-focus-wrapper"
+            key={'body'}
+            id="___gatsby"
             dangerouslySetInnerHTML={{ __html: body }}
           />
-          <div id="gatsby-announcer" />
-        </div>
-        {postBodyComponents}
-      </body>
-    </html>
-  );
-}
+          {postBodyComponents}
+        </body>
+      )}
+  </html>
+);
 
 HTML.propTypes = {
-  htmlAttributes: T.shape({
-    lang: T.string.isRequired,
-  }).isRequired,
-  headComponents: T.arrayOf(T.node).isRequired,
-  body: T.string.isRequired,
-  postBodyComponents: T.arrayOf(T.node).isRequired,
-  // bodyAttributes: T.object,
-  // preBodyComponents: T.array,
+  htmlAttributes: T.object,
+  headComponents: T.array,
+  bodyAttributes: T.object,
+  preBodyComponents: T.array,
+  body: T.string,
+  postBodyComponents: T.array,
 };
 
-export default HTML;
+export { HTML as default };
