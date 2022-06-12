1b5485e955fef158c06a3c1a5dc9b330a647a1bd
shoonia <alexanderz@wix.com>
2022-06-12 16:59:04 +0300

    Update tests

--- tests/iframe.test.js
+++ tests/iframe.test.js
@@ -1,15 +1,15 @@
 const { test } = require('uvu');
 const { is } = require('uvu/assert');
 
-const urls = {
-  'Query selector for child elements':                  'https://shoonia.wixsite.com/blog/child-selector',
-  'Promise Queue':                                      'https://alexanderz5.wixsite.com/promise-queue',
-  'Reduce server-side calls using a caching mechanism': 'https://shoonia.wixsite.com/blog/cache',
-  'Message channel to iFrame':                          'https://shoonia.wixsite.com/blog/channel',
-  'Imitating hover event on repeater container':        'https://shoonia.wixsite.com/blog/imitate-hover-event-on-corvid',
-};
+const urls = [
+  ['Query selector for child elements', 'https://shoonia.wixsite.com/blog/child-selector'],
+  ['Promise Queue', 'https://alexanderz5.wixsite.com/promise-queue'],
+  ['Reduce server-side calls using a caching mechanism', 'https://shoonia.wixsite.com/blog/cache'],
+  ['Message channel to iFrame', 'https://shoonia.wixsite.com/blog/channel'],
+  ['Imitating hover event on repeater container', 'https://shoonia.wixsite.com/blog/imitate-hover-event-on-corvid'],
+];
 
-Object.entries(urls).forEach(([title, url]) => {
+urls.forEach(([title, url]) => {
   test(title, async () => {
     const { status } = await fetch(url);
 
