3ec8674d9f7a70b72cbedd1ddf36add3e19fa5d5
shoonia <alexanderz@wix.com>
2022-06-12 11:23:50 +0300

    Add iframe tests

--- tests/iframe.test.js
+++ tests/iframe.test.js
@@ -1,10 +1,20 @@
 const { test } = require('uvu');
 const { is } = require('uvu/assert');
 
-test('Promise Queue', async () => {
-  const response = await fetch('https://alexanderz5.wixsite.com/promise-queue');
+const urls = {
+  'Query selector for child elements':                  'https://shoonia.wixsite.com/blog/child-selector',
+  'Promise Queue':                                      'https://alexanderz5.wixsite.com/promise-queue',
+  'Reduce server-side calls using a caching mechanism': 'https://shoonia.wixsite.com/blog/cache',
+  'Message channel to iFrame':                          'https://shoonia.wixsite.com/blog/channel',
+  'Imitating hover event on repeater container':        'https://shoonia.wixsite.com/blog/imitate-hover-event-on-corvid',
+};
 
-  is(response.status, 200);
+Object.entries(urls).forEach(([title, url]) => {
+  test(title, async () => {
+    const { status } = await fetch(url);
+
+    is(status, 200);
+  });
 });
 
 test.run();
