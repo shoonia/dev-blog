acb59994578a75e798060b3621b2aa5b5c280aea
shoonia <alexanderz@wix.com>
2022-07-17 16:27:51 +0300

    Add kind

--- src/posts/posts.11tydata.js
+++ src/posts/posts.11tydata.js
@@ -1,7 +1,9 @@
 const { image, authorName, jsonLd } = require('../../util/data');
+const { Kind } = require('../../util/filters');
 
 module.exports = {
   layout: 'posts.njk',
+  kind: Kind.posts,
   eleventyComputed: {
     image,
     authorName,
--- util/filters.js
+++ util/filters.js
@@ -1,6 +1,17 @@
-exports.getPosts = (items) => {
-  return items
-    .filter((i) => i.data.layout === 'posts.njk')
-    .map((i) => i.data)
-    .sort((a, b) => Date.parse(b.date) - Date.parse(a.date));
+const Kind = {
+  posts: 'posts',
+  snippet: 'snippet',
+};
+
+const sort = (a, b) => Date.parse(b.date) - Date.parse(a.date);
+const map = (i) => i.data;
+
+module.exports = {
+  Kind,
+  getPosts: (items) => {
+    return items
+      .filter((i) => i.data.kind === Kind.posts)
+      .map(map)
+      .sort(sort);
+  },
 };
