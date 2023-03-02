bfd852760446bd5d8d4d2f6a1cf344d9f8947afe
shoonia <alexanderz@wix.com>
2023-03-02 18:51:57 +0200

    Add demo page to test

--- tests/iframe.test.js
+++ tests/iframe.test.js
@@ -2,12 +2,13 @@ const { test } = require('uvu');
 const { is } = require('uvu/assert');
 
 const urls = [
-  ['Query selector for child elements', 'https://shoonia.wixsite.com/blog/child-selector'],
-  ['Promise Queue', 'https://alexanderz5.wixsite.com/promise-queue'],
-  ['Reduce server-side calls using a caching mechanism', 'https://shoonia.wixsite.com/blog/cache'],
-  ['Message channel to iFrame', 'https://shoonia.wixsite.com/blog/channel'],
-  ['Imitating hover event on repeater container', 'https://shoonia.wixsite.com/blog/imitate-hover-event-on-corvid'],
-  ['Rotate animation', 'https://shoonia.wixsite.com/blog/rotate-animation'],
+  ['Promise Queue', /*                                 */ 'https://alexanderz5.wixsite.com/promise-queue'],
+  ['Query selector for child elements', /*             */ 'https://shoonia.wixsite.com/blog/child-selector'],
+  ['Reduce server-side calls using a caching mechanism',  'https://shoonia.wixsite.com/blog/cache'],
+  ['Message channel to iFrame', /*                     */ 'https://shoonia.wixsite.com/blog/channel'],
+  ['Imitating hover event on repeater container', /*   */ 'https://shoonia.wixsite.com/blog/imitate-hover-event-on-corvid'],
+  ['Rotate animation', /*                              */ 'https://shoonia.wixsite.com/blog/rotate-animation'],
+  ['Create accordion in Velo', /*                      */ 'https://shoonia.wixsite.com/blog/toggle'],
 ];
 
 urls.forEach(([title, url]) => {
